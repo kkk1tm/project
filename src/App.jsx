495c87c01c5db89d14e89f659c1c43c2ec00f710
Misha <Mishanya_05@bk.ru>
2023-09-22 10:38:08 +0300

    hello

---
+++ src/App.jsx
@@ -0,0 +1,112 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+// ВВЕДЕНИЕ
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+//
+
+
+// ЗАДАНИЕ 7.1, 7.2, 7.3
+
+// function App() {
+// 	return <div className='text1'>
+// 		hochu otdohnut'
+//     <br></br>
+//     <div className='text2'>ochen' sil'no</div>
+// 	</div>;
+// }
+
+// ЗАДАНИЕ 8
+
+// function App() {
+// 	return	<div>
+//     <ul>
+//       <li>1</li>
+//       <li>2</li>
+//       <li>3</li>
+//       <li>4</li>
+//       <li>5</li>
+//       <li>6</li>
+//       <li>7</li>
+//       <li>8</li>
+//       <li>9</li>
+//       <li>10</li>
+//     </ul>
+//   </div>
+// }
+
+// ЗАДАНИЕ 9
+
+// function App() {
+// 	return(
+//     <div>
+// 		<ul>
+// 			<li>text1</li>
+// 			<li>text2</li>
+// 			<li>text3</li>
+// 		</ul>
+//     </div>
+//   )
+// }
+
+// ЗАДАНИЕ 10
+
+// function App() {
+// 	return(<><ul>
+// 		  <li>text1</li>
+// 		  <li>text2</li>
+// 		  <li>text3</li>
+// 	  </ul>
+// 	  <ul>
+// 		  <li>text1</li>
+// 		  <li>text2</li>
+// 		  <li>text3</li>
+// 	  </ul>;</>
+//   )
+// }
+
+// ЗАДАНИЕ 11
+
+// function App() {
+// 	return <input/>;
+// }
+
+// ЗАДАНИЕ 12
+
+// function App() {
+// 	return <><input/><input/><input/></>;
+// }
+
+
+
+export default App
